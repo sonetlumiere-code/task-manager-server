42af08da352c51c9efcc0085fa0e936efc5ae76a
sonetlumiere-code <lucas.perez.ok@gmail.com>
2023-07-10 23:21:15 -0300

    commented mongoDB connection

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 const port = process.env.PORT || 3434;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGOURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// mongoose.connect(process.env.MONGOURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
 
 // Middleware
 app.use(cors());
